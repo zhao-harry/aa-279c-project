a2085d1ba13e397b3d2f09b38530e15f87bf8951
Kuslam <kusaluprety@gmail.com>
2024-04-15 13:54:32 -0700

    updated plots based on TA feedback - separated ellipsoids - added new extra cases

--- src/ps2.m
+++ src/ps2.m
@@ -1,6 +1,18 @@
 %% Problem Set 2
 clear; close all; clc;
 
+%% Problem 9
+% Set problem9 bool and problem9axes based on if we're doing problem 9 or
+% not
+problem9 = true;
+prob9axis = 'z';
+LogicalStr = {'false', 'true'};
+fprintf("Problem 9 bool is: %s\n", LogicalStr{problem9+1})
+
+if problem9 == false
+    prob9axis = 'n';
+end
+
 %% Problem 1
 a = 7125.48662; % km
 e = 0.0011650;
@@ -76,18 +88,27 @@ saveas(gcf,'Images/ps2_model.png');
 
 %% Problem 5
 % Define w
-problem9 = false;
-LogicalStr = {'false', 'true'};
-fprintf("Problem 9 bool is: %s\n", LogicalStr{problem9+1})
-
 if ~problem9
     w0Deg = [8;4;6];
 else
-    w0Deg = [1;0;0]*8;
+    if prob9axis == 'x'
+        w0Deg = 8*[1;0;0];
+    elseif prob9axis == 'y'
+        w0Deg = 8*[0.01;1;0.01];
+    elseif prob9axis == 'z'
+        w0Deg = 8*[0.01;0;1];
+    elseif prob9axis == 'n'
+    else
+        error("select correct prob9axis")
+    end
 end
+
 w0 = deg2rad(w0Deg);
 
 tspan = 0:120;
+if prob9axis == 'y'
+    tspan = 0:1200;
+end
 options = odeset('RelTol',1e-6,'AbsTol',1e-9);
 [t,w] = ode113(@(t,w) eulerPropagator(t,w,Ix,Iy,Iz),tspan,w0,options);
 
@@ -102,7 +123,7 @@ ylabel(['Angular velocity (\omega) [' char(176) '/s]'])
 if ~problem9
     prob5Name = 'Images/ps2_euler_equations.png';
 else
-    prob5Name = 'Images/ps2_problem9_euler_equations.png';
+    prob5Name = ['Images/ps2_problem9_euler_equations_', prob9axis, '.png'];
 end
 saveas(1,prob5Name)
 
@@ -113,22 +134,52 @@ L = sqrt(sum((w0.*IPrincipal).^2,"all"));
 [XE,YE,ZE] = ellipsoid(0,0,0,sqrt(2*T/Ix),sqrt(2*T/Iy),sqrt(2*T/Iz),50);
 [XM,YM,ZM] = ellipsoid(0,0,0,L/Ix,L/Iy,L/Iz,50);
 
+ellipsAxes = [sqrt(2*T/Ix), sqrt(2*T/Iy), sqrt(2*T/Iz)];
+momAxes = [L/Ix, L/Iy, L/Iz];
+
+% Plot energy ellipsoid
+figure(1)
 energyEllipsoid = surf(XE,YE,ZE,'FaceAlpha',0.5,'FaceColor','blue','DisplayName','Energy Ellipsoid');
+axis equal
+hold on
+quiver3(0, 0, 0, ellipsAxes(1), 0, 0, 'Color', 'r', 'LineWidth', 2)
+quiver3(0, 0, 0, 0, ellipsAxes(2), 0, 'Color', 'r', 'LineWidth', 2)
+quiver3(0, 0, 0, 0, 0, ellipsAxes(3), 'Color', 'r', 'LineWidth', 2)
 xlabel('\omega_{x} [rad/s]')
 ylabel('\omega_{y} [rad/s]')
 zlabel('\omega_{z} [rad/s]')
-axis equal
-hold on
-momentumEllipsoid = surf(XM,YM,ZM,'FaceAlpha',0.5,'FaceColor','green','DisplayName','Momentum Ellipsoid');
-legend('Location','northwest')
 hold off
 
 if ~problem9
-    prob6Name = 'Images/ps2_problem6.png';
+    prob6Name = 'Images/ps2_problem6_energy.png';
 else
-    prob6Name = 'Images/ps2_problem9_p6.png';
+    prob6Name = ['Images/ps2_problem9_p6_energy_', prob9axis, '.png'];
 end
 saveas(1,prob6Name)
+
+% Plot momentum ellipsoid
+figure(1)
+momentumEllipsoid = surf(XM,YM,ZM,'FaceAlpha',0.5,'FaceColor','green','DisplayName','Momentum Ellipsoid');
+hold on
+quiver3(0, 0, 0, momAxes(1), 0, 0, 'Color', 'r', 'LineWidth', 2)
+quiver3(0, 0, 0, 0, momAxes(2), 0, 'Color', 'r', 'LineWidth', 2)
+quiver3(0, 0, 0, 0, 0, momAxes(3), 'Color', 'r', 'LineWidth', 2)
+hold off
+
+if ~problem9
+    prob6Name = 'Images/ps2_problem6_momentum.png';
+else
+    prob6Name = ['Images/ps2_problem9_momentum_', prob9axis, '.png'];
+end
+saveas(1,prob6Name)
+
+verifier = L^2/(2*T);
+if Ix <= verifier && verifier <= Iz
+    fprintf("The polhode is real!\n")
+else
+    error("The polhode is NOT real!\n")
+end
+
 
 %% Problem 7
 energyEllipsoid = surf(XE,YE,ZE,'FaceAlpha',0.5,'FaceColor','blue','DisplayName','Energy Ellipsoid');
@@ -145,35 +196,58 @@ hold off
 if ~problem9
     prob7Name = 'Images/ps2_problem7.png';
 else
-    prob7Name = 'Images/ps2_problem9_p7.png';
+    prob7Name = ['Images/ps2_problem9_p7_', prob9axis, '.png'];
 end
 saveas(1,prob7Name)
 
 %% Problem 8
-subplot(1,3,1)
-plot(w(:,2),w(:,3),'Marker','o')
-title('Polhode (along x-axis)')
-xlabel('\omega_{y} [rad/s]')
-ylabel('\omega_{z} [rad/s]')
-axis equal
-
-subplot(1,3,2)
-plot(w(:,1),w(:,3),'Marker','o')
-title('Polhode (along y-axis)')
-xlabel('\omega_{x} [rad/s]')
-ylabel('\omega_{z} [rad/s]')
-axis equal
-
-subplot(1,3,3)
-plot(w(:,1),w(:,2),'Marker','o')
-title('Polhode (along z-axis)')
-xlabel('\omega_{x} [rad/s]')
-ylabel('\omega_{y} [rad/s]')
-axis equal
+if prob9axis == 'n' || prob9axis == 'y' || prob9axis == 'z'
+    subplot(1,3,1)
+    plot(w(:,2),w(:,3))
+    title('Polhode (along x-axis)')
+    xlabel('\omega_{y} [rad/s]')
+    ylabel('\omega_{z} [rad/s]')
+    axis equal
+    
+    subplot(1,3,2)
+    plot(w(:,1),w(:,3))
+    title('Polhode (along y-axis)')
+    xlabel('\omega_{x} [rad/s]')
+    ylabel('\omega_{z} [rad/s]')
+    axis equal
+    
+    subplot(1,3,3)
+    plot(w(:,1),w(:,2))
+    title('Polhode (along z-axis)')
+    xlabel('\omega_{x} [rad/s]')
+    ylabel('\omega_{y} [rad/s]')
+    axis equal
+else
+    subplot(1,3,1)
+    plot(w(:,2),w(:,3),'Marker','o')
+    title('Polhode (along x-axis)')
+    xlabel('\omega_{y} [rad/s]')
+    ylabel('\omega_{z} [rad/s]')
+    axis equal
+    
+    subplot(1,3,2)
+    plot(w(:,1),w(:,3),'Marker','o')
+    title('Polhode (along y-axis)')
+    xlabel('\omega_{x} [rad/s]')
+    ylabel('\omega_{z} [rad/s]')
+    axis equal
+    
+    subplot(1,3,3)
+    plot(w(:,1),w(:,2),'Marker','o')
+    title('Polhode (along z-axis)')
+    xlabel('\omega_{x} [rad/s]')
+    ylabel('\omega_{y} [rad/s]')
+    axis equal
+end
 
 if ~problem9
     prob8Name = 'Images/ps2_problem8.png';
 else
-    prob8Name = 'Images/ps2_problem9_p8.png';
+    prob8Name = ['Images/ps2_problem9_p8_,' prob9axis, '.png'];
 end
 saveas(1,prob8Name)
