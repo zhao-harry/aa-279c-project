f60e9d817f7eec4521a769549835a374de62a790
zhao-harry <68164792+zhao-harry@users.noreply.github.com>
2024-04-17 18:51:50 -0700

    PS3 problems 1-3

--- src/ps3.m
+++ src/ps3.m
@@ -8,7 +8,31 @@ Iz = IPrincipal(3,3);
 
 w0Deg = [8;4;6];
 w0 = deg2rad(w0Deg);
-tspan = 0:120;
+tspan = 0:0.1:120;
 w = eulerPropagator(w0,Ix,Iy,Iz,tspan,'Images/ps3_problem1.png');
 
 %% Problem 2
+lambda = w0(3) * (Iz - Iy) / Ix;
+wxy = (w0(1) + w0(2) * 1j) * exp(1j * lambda * tspan);
+wx = real(wxy);
+wy = imag(wxy);
+wz = w0(3) * ones(size(wxy));
+wAnalytical = [wx',wy',wz'];
+wDegAnalytical = rad2deg(wAnalytical);
+
+figure(1)
+plot(tspan,wDegAnalytical,'LineWidth',2)
+legend('\omega_{x}','\omega_{y}','\omega_{z}', ...
+    'Location','southeast')
+xlabel('Time [s]')
+ylabel(['Angular velocity (\omega) [' char(176) '/s]'])
+saveas(1,'Images/ps3_problem2.png')
+
+%% Problem 3
+error = w - wAnalytical;
+plot(tspan,error,'LineWidth',2)
+legend('\omega_{x}','\omega_{y}','\omega_{z}', ...
+    'Location','southeast')
+xlabel('Time [s]')
+ylabel('Angular velocity (\omega) [rad/s]')
+saveas(gcf,'Images/ps3_problem3.png')
