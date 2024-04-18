6d2cdf9af681dd50c8c106ba8e5d1a9c66687df3
zhao-harry <68164792+zhao-harry@users.noreply.github.com>
2024-04-17 17:30:00 -0700

    Update PS2 submission

--- src/eulerPropagator.m
+++ src/eulerPropagator.m
@@ -5,7 +5,8 @@ function w = eulerPropagator(w0,Ix,Iy,Iz,tspan,filename)
 
     figure(1)
     plot(t,wDeg,'LineWidth',2)
-    legend('\omega_{x}','\omega_{y}','\omega_{z}','Location','southeast')
+    legend('\omega_{x}','\omega_{y}','\omega_{z}', ...
+        'Location','southeast')
     xlabel('Time [s]')
     ylabel(['Angular velocity (\omega) [' char(176) '/s]'])
     saveas(1,filename)
