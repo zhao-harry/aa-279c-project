bf141a618f772fe2fec4d40502e8c22d824273db
Kuslam <kusaluprety@gmail.com>
2024-04-17 23:49:56 -0700

    added euler stepper graphing

--- src/ps3.m
+++ src/ps3.m
@@ -36,3 +36,68 @@ legend('\omega_{x}','\omega_{y}','\omega_{z}', ...
 xlabel('Time [s]')
 ylabel('Angular velocity (\omega) [rad/s]')
 saveas(gcf,'Images/ps3_problem3.png')
+
+%% Problem 6
+% Quaternions
+% Initial angles
+e0 = [1; 0; 0];
+phi0 = pi/2;
+q0 = axisAngle2Quat(e0, phi0);
+% state = [w0; q0];
+
+tf = 60;
+dt = 0.0001;
+quats = kinQuaternionStepper(w0, q0, tf, dt, Ix, Iy, Iz);
+
+% ode113 section that doesn't work
+% % Numerically propogate quaternions
+% tspan = 0:60;
+% options = odeset('RelTol',1e-6,'AbsTol',1e-9);
+% [t,state] = ode113(@(t,w) kinQuaternion(t,state,Ix,Iy,Iz),tspan,state,options);
+
+% quats = state(:,4:end);
+figure(1)
+hold on
+plot(t, quats,'LineWidth',1)
+legend('q_{1}','q_{2}','q_{3}', 'q_{4}', ...
+plot(tspan,wDegAnalytical,'LineWidth',2)
+legend('\omega_{x}','\omega_{y}','\omega_{z}', ...
+    'Location','southeast')
+xlabel('Time [s]')
+ylabel(['Angular velocity (\omega) [' char(176) '/s]'])
+hold off
+% saveas(1,filename)
+
+%% 
+% Euler Angles
+phi0 = deg2rad(5);
+theta0 = deg2rad(5);
+psi0 = deg2rad(5);
+state = [w0; phi0; theta0; psi];
+
+% NOTE: ode113 method doesn't seem to work
+% tspan = 0:60;
+% options = odeset('RelTol',1e-6,'AbsTol',1e-9);
+% [t,state] = ode113(@(t,w) kinEulerAngle(t,state,Ix,Iy,Iz),tspan,state,options);
+% 
+% eulerAngs = wrapTo360(rad2deg(state(:,4:end)));
+
+% Stepper
+tf = 60;
+dt = 0.0001;
+eulerAngs = kinEulerAngleStepper(w0, phi0, theta0, psi0, tf, dt, Ix, Iy, Iz);
+t = 0:dt:tf;
+% saveas(1,'Images/ps4_problem6.png')
+
+figure(2)
+plot(t, eulerAngs,'LineWidth',1)
+legend('\phi','\theta','\psi', ...
+%% Problem 3
+error = w - wAnalytical;
+plot(tspan,error,'LineWidth',2)
+legend('\omega_{x}','\omega_{y}','\omega_{z}', ...
+    'Location','southeast')
+xlabel('Time [s]')
+ylabel(['Angular velocity (\omega) [' char(176) '/s]'])
+ylabel('Angular velocity (\omega) [rad/s]')
+saveas(gcf,'Images/ps3_problem3.png'
