c83d16505b164cc97dd8aa02a088fb84afd1e230
Kuslam <kusaluprety@gmail.com>
2024-04-25 20:13:59 -0700

    Create ps4.m
    created ps4

---
+++ src/ps4.m
@@ -0,0 +1,10 @@
+close all; clear; clc
+
+%% Problem 1
+cm = computeCM('res/mass.csv');
+I = computeMOI('res/mass.csv',cm);
+
+[rot,IPrincipal] = eig(I);
+Ix = IPrincipal(1,1);
+Iy = IPrincipal(2,2);
+Iz = IPrincipal(3,3);
