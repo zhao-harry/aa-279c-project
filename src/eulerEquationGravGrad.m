ff09baab33582df15f5f58e558e68ec42bcd2d48
Kuslam <kusaluprety@gmail.com>
2024-04-28 21:41:52 -0700

    added code for 4b

---
+++ src/eulerEquationGravGrad.m
@@ -0,0 +1,11 @@
+function stateDot = eulerEquationGravGrad(t,state,Ix,Iy,Iz,cx,cy,cz,n)
+    % Computes state derivatives for angular velocity with gravity gradient
+    wx = state(1);
+    wy = state(2);
+    wz = state(3);
+
+    % Angular time velocities
+    stateDot(1) = (Iz-Iy)/Ix * (3*n^2*cy*cz - wy*wz);
+    stateDot(2) = (Ix-Iz)/Iy * (3*n^2*cz*cx - wz*wx);
+    stateDot(3) = (Iy-Ix)/Iz * (3*n^2*cx*cy - wx*wy);
+end
