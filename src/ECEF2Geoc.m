f294a5e682c0d8cb35c9c12038d1dc2138d4c514
zhao-harry <68164792+zhao-harry@users.noreply.github.com>
2024-05-06 12:40:35 -0700

    Fix ECEF2Geoc spaces

--- src/ECEF2Geoc.m
+++ src/ECEF2Geoc.m
@@ -1,7 +1,7 @@
 function geoc = ECEF2Geoc(rECEF,RE)
     % RE = 6378.1;
-    lat = asind ( rECEF(3) / norm(rECEF) ) ;
-    lon = atan2d ( rECEF(2) , rECEF(1) ) ;
-    alt = norm ( rECEF ) - RE ;
-    geoc = [ lat , lon , alt ];
+    lat = asind(rECEF(3) / norm(rECEF));
+    lon = atan2d(rECEF(2),rECEF(1));
+    alt = norm(rECEF) - RE;
+    geoc = [lat,lon,alt];
 end
