65db4d58af7885bf3a77c376c023a09994779b9e
zhao-harry <68164792+zhao-harry@users.noreply.github.com>
2024-05-07 00:03:07 -0700

    Clean up getdPdTheta

--- src/getdPdTheta.m
+++ src/getdPdTheta.m
@@ -1,17 +1,21 @@
-function dPdTheta = getdPdTheta(theta, n, m)
+function dPdTheta = getdPdTheta(theta,n,m)
     if n < m
         error("n >= m for Legendre functions")
     end
     if n == m && m == 0
         dPdTheta = 0;
     elseif n == m
-        dPdTheta = sin(theta)*getdPdTheta(theta, n-1, n-1) + cos(theta)*getPnm(theta, n-1, n-1);
+        dPdTheta = sin(theta) * getdPdTheta(theta,n-1,n-1) + ...
+            cos(theta) * getPnm(theta,n-1,n-1);
     else
         K = getKnm(n,m);
         if K == 0
-            dPdTheta = cos(theta)*getdPdTheta(theta, n-1, m) - sin(theta)*getPnm(theta, n-1, m);
+            dPdTheta = cos(theta) * getdPdTheta(theta,n-1,m) - ...
+                sin(theta) * getPnm(theta,n-1,m);
         else
-            dPdTheta = cos(theta)*getdPdTheta(theta, n-1, m) - sin(theta)*getPnm(theta, n-1, m) - K*getdPdTheta(theta, n-2, m);
+            dPdTheta = cos(theta) * getdPdTheta(theta,n-1,m) - ...
+                sin(theta) * getPnm(theta,n-1,m) - ...
+                K * getdPdTheta(theta,n-2,m);
         end
     end
 end
