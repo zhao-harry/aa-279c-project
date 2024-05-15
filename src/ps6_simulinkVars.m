ba282955adf53bc809d72489a08a2c2d6a4b803e
zhao-harry <68164792+zhao-harry@users.noreply.github.com>
2024-05-14 23:08:47 -0700

    New A2q and q2A

--- src/ps6_simulinkVars.m
+++ src/ps6_simulinkVars.m
@@ -27,11 +27,7 @@ h = cross(r0,v0);
 radial = r0 / norm(r0);
 normal = h / norm(h);
 tangential = cross(normal,radial);
-A_ECI2RTN = [radial tangential normal]';
-A_RTN2ideal = [-1 0 0;
-                        0 0 -1;
-                        0 -1 0];
-A_ideal0 = A_RTN2ideal*A_ECI2RTN;
+A_Nominal = [-radial -normal -tangential]';
 
 % Earth orbit initial conditions
 aE = 149.60E6; % km
@@ -54,12 +50,12 @@ cmP = rot' * cm;
 % Parameters
 CD = 2;
 Cd = 0; Cs = 0.9;
-P = 1358/3E8;
+P = 1358 / 3e8;
 S_sat = 24.92;
-m_max = 4*pi*1e-7 * S_sat * 0.1;
+m_max = 4e-7 * pi * S_sat * 0.1;
 m_direction_body = [1; 0; 0];
 m_direction = rot * m_direction_body;
-m = m_max*m_direction/norm(m_direction); % Arbitrarily defined satellite dipole for now
+m = m_max * m_direction / norm(m_direction); % Arbitrary sat dipole
 UT1 = [2024 1 1];
 
 % Get simulink vars
@@ -67,7 +63,7 @@ constants = struct();
 constants.Ix = Ix; constants.Iy = Iy; constants.Iz = Iz;
 constants.RE = 6378.1; %km
 constants.n = n;
-constants.cm = cm;
+constants.cm = cmP;
 constants.barycenter = barycenter;
 constants.normal = normal;
 constants.area = area;
@@ -81,14 +77,14 @@ constants_bus_info = Simulink.Bus.createObject(constants);
 constants_bus = evalin('base', constants_bus_info.busName);
 rECI0 = r0; vECI0 = v0;
 rSCI0 = ySun(1:3); vSCI0 = ySun(4:6);
-q0 = A2q(A_ideal0);
-w0 = [0, 0, n];
+q0 = A2q(A_Nominal);
+w0 = [0, -n, 0];
 
 %% Plot
 eulerAngs = nan(3,length(out.q.time));
 
 for n = 1:length(eulerAngs)
-    eulerAngs(:,n) = A2e(q2A(out.q.data(:,:,n)));
+    eulerAngs(:,n) = A2e(out.A.data(:,:,n));
 end
 
 plot(out.q.time, eulerAngs)
