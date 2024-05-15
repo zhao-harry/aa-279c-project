2c21c5a17543ab7595e3d0ad62aa1e451a2a823a
Kuslam <kusaluprety@gmail.com>
2024-05-14 17:37:03 -0700

    heading to softball will finish later

--- src/qMethod.m
+++ src/qMethod.m
@@ -1,2 +1,25 @@
-function A = qMethod(m, v)
+function q = qMethod(m, v, w)
+    % Implement the Deterministic q method
+    % Inputs:
+    % - m: measured vectors
+    % - v: ground truth vector
+    % - w: weights of sensor measurements
+    % Outputs:
+    % - A: DCM between principal axes and ECI
+
+    U = sqrt(w) .* v;
+    W = sqrt(w) .* m;
+
+    B = W * U';
+    S = B + B';
+    Z = [B(2,3)-B(3,2), B(3,1)-B(1,3), B(1,2)-B(2,1)]';
+    sigma = trace(B);
+
+    K = [S - eye(size(S))*sigma, Z;
+            Z', sigma];
+
+    [lambda, q] = eig(K);
+
+    nMax = max(diag(lambda));
+    
 end
