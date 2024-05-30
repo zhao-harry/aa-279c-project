39c979be6c6b7d3b30aa7b0de3edf3f338ebda97
Kuslam <kusaluprety@gmail.com>
2024-05-29 20:51:36 -0700

    covariance errors

--- src/ps8.m
+++ src/ps8.m
@@ -1,9 +1,8 @@
 %% Model
 close all; clear; clc
+savePlots = false;
 
 %%  Plots
-savePlots = true;
-
 qVals = squeeze(out.q.data);
 qMeasVals = squeeze(out.qMeasured.data);
 wVals = squeeze(out.w.data);
@@ -29,7 +28,7 @@ xlabel('Time [s]')
 ylabel('Residual norms')
 legend('pre-fit','post-fit')
 hold off
-saveAsBool(gcf,'Images/ps8_problem7_res_units.png',savePlots)
+saveAsBool(gcf,'Images/ps8_problem7_res_units.png', savePlots)
 
 figure()
 hold on
@@ -39,7 +38,7 @@ xlabel('Time [s]')
 ylabel('Residual norms')
 legend('pre-fit','','','post-fit')
 hold off
-saveAsBool(gcf,'Images/ps8_problem7_res_gyro.png',savePlots)
+saveAsBool(gcf,'Images/ps8_problem7_res_gyro.png', savePlots)
 
 figure()
 hold on
@@ -49,7 +48,7 @@ xlabel('Time [s]')
 ylabel('Euler Angle (Principal) [rad]')
 legend('MEKF','','','','Ground Truth')
 hold off
-saveAsBool(gcf,'Images/ps8_problem7_state.png',savePlots)
+saveAsBool(gcf,'Images/ps8_problem7_state.png', savePlots)
 
 % Get error
 figure()
@@ -67,10 +66,16 @@ xlim([0 timeVals(end)/3600])
 xlabel("time [hr]")
 ylabel("euler angle [deg]")
 legend("\phi", "\theta", "\psi")
-saveAsBool(gcf,'Images/ps8_problem7_error.png',savePlots)
+saveAsBool(gcf,'Images/ps8_problem7_error.png', savePlots)
 
-% figure()
-% hold on
-% plot(timeValsMeas, wMeasVals, 'b')
-% plot(timeVals, wVals, 'r--')
-% hold off
+%%
+figure()
+covError = [squeeze(out.Pkplus.Data(1,1,:)), ...
+                  squeeze(out.Pkplus.Data(2,2,:)), ...
+                  squeeze(out.Pkplus.Data(3,3,:))]';
+plot(timeVals/3600, rad2deg(covError))
+xlim([0 timeVals(end)/3600])
+xlabel("time [hr]")
+ylabel("euler angle [deg]")
+legend("\phi", "\theta", "\psi")
+saveAsBool(gcf,'Images/ps8_problem7_cov.png', savePlots)
