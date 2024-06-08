de034bb8de1a3cfce565ce2b0a9247ceda4ce0c6
Kuslam <kusaluprety@gmail.com>
2024-06-07 18:25:55 -0700

    saturation

--- src/modelVars.m
+++ src/modelVars.m
@@ -129,6 +129,7 @@ sensors_bus = evalin('base', sensors_bus_info.busName);
 % Actuators
 IWheel = 0.119; % kg*m^2
 LMaxWheel = 50; %N*m*s
+LMaxWheel = IWheel * 1; %N*m*s
 dipole = [350; 350; 565]; %A*m^2
 thrust = 1; % N
 Isp = 220; %s
