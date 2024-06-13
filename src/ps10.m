41e7c6ba851dca79fba96b35e2cb1ab82bfd6252
zhao-harry <68164792+zhao-harry@users.noreply.github.com>
2024-06-12 17:06:17 -0700

    Add angvel and error plots for desaturation

--- src/ps10.m
+++ src/ps10.m
@@ -2,7 +2,7 @@ close all; clear; clc;
 savePlots = false;
 modelVars
 
-%% Plotting
+%% Angular Velocity Plot
 time = squeeze(out.q.time);
 Lw = squeeze(out.Lw.Data);
 w_wheel = Lw ./ control.IWheel;
@@ -13,9 +13,19 @@ for n = 1:4
     hold on
     title(sprintf("Wheel %i", n))
     
-    yyaxis left
     ylabel("Angular Velocity [rad/s]")
     plot(time, w_wheel(n,:), 'b')
     ylim([min(w_wheel(:)), max(w_wheel(:))])
     xlabel("Time [s]")
 end
+
+%% Control Error Plot
+eulerControlError = A2eVec(out.AE.data);
+figure()
+hold on
+plot(time/3600, rad2deg(eulerControlError))
+xlabel("Time [hr]"); ylabel("Euler Angles [deg]")
+legend(["\phi", "\theta", "\psi"])
+title("Attitude Control Error")
+xlim([0, time(end)/3600])
+hold off
